adfa6c123bbda9a4d799077493786c3db85ff833
Samuel Dacanay <sam.dacanay@anchore.com>
2021-02-05 16:08:34 -0800

    Add integration test suite, complete with k8s cluster installation, and improve acceptance test script/workflow
    Signed-off-by: Samuel Dacanay <sam.dacanay@anchore.com>

---
+++ test/integration/get_images_test.go
@@ -0,0 +1,55 @@
+package integration
+
+import (
+	"github.com/anchore/kai/cmd"
+	"github.com/anchore/kai/kai"
+	"strings"
+	"testing"
+)
+
+const IntegrationTestNamespace = "kai-integration-test"
+const IntegrationTestImageTag = "nginx:latest"
+
+// Assumes that the hello-world helm chart in ./fixtures was installed (basic nginx container)
+func TestGetImageResults(t *testing.T) {
+	cmd.InitAppConfig()
+	imagesResult, err := kai.GetImageResults(cmd.GetAppConfig())
+	if err != nil {
+		t.Fatalf("failed to get image results")
+	}
+
+	if imagesResult.ServerVersionMetadata == nil {
+		t.Errorf("Failed to include Server Version Metadata in result")
+	}
+
+	if imagesResult.Timestamp == "" {
+		t.Errorf("Failed to include Timestamp in result")
+	}
+
+	foundIntegrationTestNamespace := false
+	for _, namespace := range imagesResult.Results {
+		if namespace.Namespace != IntegrationTestNamespace {
+			continue
+		} else {
+			foundIntegrationTestNamespace = true
+			foundIntegrationTestImage := false
+			for _, image := range namespace.Images {
+				if !strings.Contains(image.Tag, IntegrationTestImageTag) {
+					continue
+				} else {
+					foundIntegrationTestImage = true
+					if image.RepoDigest == "" {
+						t.Logf("Image Found, but no digest located: %v", image)
+					}
+				}
+			}
+			if !foundIntegrationTestImage {
+				t.Errorf("failed to locate integration test image")
+			}
+		}
+	}
+	if !foundIntegrationTestNamespace {
+		t.Errorf("failed to locate integration test namespace")
+	}
+
+}
