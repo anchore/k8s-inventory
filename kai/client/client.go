5a7fd953ef8b4d351c62f1e848deb5f3484b88c2
Rogerio Bastos <2397391+rogeriobastos@users.noreply.github.com>
2021-08-13 10:14:50 -0300

    add modeule k8s.io/client-go/plugin/pkg/client/auth

--- kai/client/client.go
+++ kai/client/client.go
@@ -10,6 +10,7 @@ import (
 	"github.com/anchore/kai/internal/config"
 	"github.com/mitchellh/go-homedir"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
