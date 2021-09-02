52c05d3c1d5e9432e2b1e3c6a6dd422a3063a7c7
Rogerio Bastos <2397391+rogeriobastos@users.noreply.github.com>
2021-09-02 15:32:30 -0300

    change report func to be compatible with open-source version of anchore engine

--- kai/reporter/reporter.go
+++ kai/reporter/reporter.go
@@ -15,20 +15,51 @@ import (
 	"github.com/anchore/kai/kai/result"
 )
 
-const ReportAPIPath = "v1/enterprise/inventories"
+const ReportAPIPath = "/v1/images"
 
-type InventoryReport struct {
-	result.Result
-	ClusterName   string `json:"cluster_name"`
-	InventoryType string `json:"inventory_type"`
+type ImageReport struct {
+	Source Source `json:"source"`
 }
 
-func NewInventoryReport(result result.Result, clusterName string) *InventoryReport {
-	return &InventoryReport{
-		result,
-		clusterName,
-		"kubernetes",
+type Source struct {
+	Digest Digest `json:"digest"`
+}
+
+type Digest struct {
+	PullString string `json:"pullstring"`
+	Tag        string `json:"tag"`
+	Timestamp  string `json:"creation_timestamp_override"`
+}
+
+func stripTagName(tag string) string {
+	for i, v := range tag {
+		if v == ':' || v == '@' {
+			return tag[:i]
+		}
 	}
+	return tag
+}
+
+func newPullString(tag, digest string) string {
+	return fmt.Sprintf("docker.io/%s@%s", stripTagName(tag), digest)
+}
+
+func NewImageReport(result result.Result) (report []ImageReport) {
+	for _, namespace := range result.Results {
+		for _, image := range namespace.Images {
+			// TODO: handle docker.io tags
+			report = append(report, ImageReport{
+				Source: Source{
+					Digest: Digest{
+						PullString: newPullString(image.Tag, image.RepoDigest),
+						Tag: image.Tag,
+						Timestamp: result.Timestamp,
+					},
+				},
+			})
+		}
+	}
+	return report
 }
 
 // This method does the actual Reporting (via HTTP) to Anchore
@@ -48,25 +79,28 @@ func Report(result result.Result, anchoreDetails config.AnchoreInfo, appConfig *
 		return fmt.Errorf("failed to build url: %w", err)
 	}
 
-	reqBody, err := json.Marshal(NewInventoryReport(result, appConfig.KubeConfig.Cluster))
-	if err != nil {
-		return fmt.Errorf("failed to serialize results as JSON: %w", err)
-	}
+	for i, image := range NewImageReport(result) {
+		log.Debug("Reporting image ", i)
+		reqBody, err := json.Marshal(image)
+		if err != nil {
+			return fmt.Errorf("failed to serialize results as JSON: %w", err)
+		}
 
-	req, err := http.NewRequest("POST", anchoreURL, bytes.NewBuffer(reqBody))
-	if err != nil {
-		return fmt.Errorf("failed to build request to report data to Anchore: %w", err)
-	}
-	req.SetBasicAuth(anchoreDetails.User, anchoreDetails.Password)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("x-anchore-account", anchoreDetails.Account)
-	resp, err := client.Do(req)
-	if err != nil {
-		return fmt.Errorf("failed to report data to Anchore: %w", err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("failed to report data to Anchore: %+v", resp)
+		req, err := http.NewRequest("POST", anchoreURL, bytes.NewBuffer(reqBody))
+		if err != nil {
+			return fmt.Errorf("failed to build request to report data to Anchore: %w", err)
+		}
+		req.SetBasicAuth(anchoreDetails.User, anchoreDetails.Password)
+		req.Header.Set("Content-Type", "application/json")
+		//req.Header.Set("x-anchore-account", anchoreDetails.Account)
+		resp, err := client.Do(req)
+		if err != nil {
+			return fmt.Errorf("failed to report data to Anchore: %w", err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != 200 {
+			return fmt.Errorf("failed to report data to Anchore: %+v", resp)
+		}
 	}
 	log.Debug("Successfully reported results to Anchore")
 	return nil
