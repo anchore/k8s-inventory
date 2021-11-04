3fb34ab2818ac867938c54d55a0dc5e3d0d75b6f
James Petersen <jpetersenames@gmail.com>
2021-11-04 17:14:14 -0600

    add in some base code for parsing out the tag/digest
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- kai/inventory/reportitem.go
+++ kai/inventory/reportitem.go
@@ -2,34 +2,24 @@ package inventory
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
+
+	"github.com/anchore/kai/internal/log"
 
 	v1 "k8s.io/api/core/v1"
 )
 
-// Represents a ReportItem Images list result
+// ReportItem represents a ReportItem Images list result
 type ReportItem struct {
 	Namespace string        `json:"namespace,omitempty"`
 	Images    []ReportImage `json:"images"`
 }
 
+// ReportImage represents a unique image in a cluster
 type ReportImage struct {
 	Tag        string `json:"tag,omitempty"`
 	RepoDigest string `json:"repoDigest,omitempty"`
-}
-
-func NewFromPod(pod v1.Pod) *ReportItem {
-	return &ReportItem{
-		Namespace: pod.Namespace,
-		Images:    getUniqueImagesFromPodStatus(pod),
-	}
-}
-
-func New(namespace string) *ReportItem {
-	return &ReportItem{
-		Namespace: namespace,
-		Images:    []ReportImage{},
-	}
 }
 
 // NewReportItem parses a list of pods into a ReportItem full of unique images
@@ -45,7 +35,7 @@ func NewReportItem(pods []v1.Pod, namespace string) ReportItem {
 		if namespace == "" || len(pod.Status.ContainerStatuses) == 0 {
 			continue
 		}
-		reportItem.AddImages(pod)
+		reportItem.extractUniqueImages(pod)
 	}
 
 	return reportItem
@@ -57,21 +47,22 @@ func (r *ReportItem) String() string {
 }
 
 // Adds an ReportImage to the ReportItem struct (if it doesn't exist there already)
-func (r *ReportItem) AddImages(pod v1.Pod) {
+func (r *ReportItem) extractUniqueImages(pod v1.Pod) {
 
 	if len(r.Images) == 0 {
-		r.Images = getUniqueImagesFromPodStatus(pod)
+		r.Images = process(pod)
 
 	} else {
 
 		// Build a Map to make use as a Set (unique list). Values are empty structs so they don't waste space
 		imageSet := make(map[string]ReportImage)
 		for _, image := range r.Images {
-			// There's always a tag, the repoDigest may be missing
+			// TODO: Use the image:tag@digest to test for uniqueness
 			imageSet[image.Tag] = image
 		}
+
 		// If the image isn't in the set already, append it to the list
-		for _, image := range getUniqueImagesFromPodStatus(pod) {
+		for _, image := range process(pod) {
 			if _, ok := imageSet[image.Tag]; !ok {
 				r.Images = append(r.Images, image)
 			}
@@ -79,27 +70,162 @@ func (r *ReportItem) AddImages(pod v1.Pod) {
 	}
 }
 
-func getUniqueImagesFromPodStatus(pod v1.Pod) []ReportImage {
-	imageMap := make(map[string]ReportImage)
-	for _, container := range pod.Status.ContainerStatuses {
-		repoDigest := getImageDigest(container.ImageID)
-		imageMap[container.Image] = ReportImage{
-			Tag:        container.Image,
-			RepoDigest: repoDigest,
-		}
-	}
-	imageSlice := make([]ReportImage, 0, len(imageMap))
-	for _, v := range imageMap {
-		imageSlice = append(imageSlice, v)
-	}
-	return imageSlice
+const regexTag = `:[\w][\w.-]{0,127}$`
+
+type Image struct {
+	digest string
+	tag    string
+	image  string
+	key    string
 }
 
-func getImageDigest(imageID string) string {
-	var imageDigest = ""
-	// If the image ID contains "sha", it corresponds to the repo digest. If not, it's not a digest
-	if strings.Contains(imageID, "@") {
-		imageDigest = strings.Split(imageID, "@")[1]
+type Spec struct {
+	image string
+}
+
+type Status struct {
+	image   string
+	imageID string
+}
+
+type data struct {
+	spec   Spec
+	status Status
+}
+
+func fill(pod v1.Pod) map[string]data {
+	cmap := make(map[string]data)
+
+	// grab init images
+	for _, c := range pod.Spec.InitContainers {
+		cmap[c.Name] = data{
+			spec: Spec{
+				image: c.Image,
+			},
+		}
 	}
-	return imageDigest
+
+	for _, c := range pod.Status.InitContainerStatuses {
+		mm, exists := cmap[c.Name]
+		spec := Spec{}
+
+		if exists {
+			spec = mm.spec
+		}
+
+		cmap[c.Name] = data{
+			spec: spec,
+			status: Status{
+				image:   c.Image,
+				imageID: c.ImageID,
+			},
+		}
+	}
+
+	// grab regular images
+	for _, c := range pod.Spec.Containers {
+		cmap[c.Name] = data{
+			spec: Spec{
+				image: c.Image,
+			},
+		}
+	}
+
+	for _, c := range pod.Status.ContainerStatuses {
+		mm, exists := cmap[c.Name]
+		spec := Spec{}
+
+		if exists {
+			spec = mm.spec
+		}
+
+		cmap[c.Name] = data{
+			spec: spec,
+			status: Status{
+				image:   c.Image,
+				imageID: c.ImageID,
+			},
+		}
+	}
+	return cmap
+}
+
+func parseout(s string, c *Image) error {
+
+	if c.digest != "" && c.tag != "" && c.image != "" {
+		log.Info("all fields have been parsed")
+		return nil
+	}
+
+	reg, err := regexp.Compile(regexTag)
+	if err != nil {
+		return err
+	}
+
+	image := s
+	digest := ""
+	if strings.Contains(s, "@") {
+		split := strings.Split(s, "@")
+		image = split[0]
+		digest = split[1]
+	}
+
+	tag := ""
+	m := reg.FindAllString(image, -1)
+	if len(m) > 0 {
+		i := strings.LastIndex(image, ":")
+		image = image[0:i]
+		tag = strings.TrimPrefix(m[0], ":")
+	}
+
+	if c.digest == "" {
+		c.digest = digest
+	}
+	if c.tag == "" {
+		c.tag = tag
+	}
+	if c.image == "" {
+		c.image = image
+	}
+	return nil
+}
+
+func extract(d data) (Image, error) {
+
+	c := Image{
+		image:  "",
+		tag:    "",
+		digest: "",
+		key:    "",
+	}
+
+	parseout(d.spec.image, &c)
+	parseout(d.status.image, &c)
+	parseout(d.status.imageID, &c)
+
+	c.key = fmt.Sprintf("%s:%s@%s", c.image, c.tag, c.digest)
+	return c, nil
+}
+
+func process(pod v1.Pod) []ReportImage {
+	cmap := fill(pod)
+
+	unique := make(map[string]Image)
+	for _, n := range cmap {
+		c, err := extract(n)
+		if err != nil {
+			log.Errorf("issue processing %s", n)
+			continue
+		}
+		unique[c.key] = c
+	}
+
+	ri := make([]ReportImage, 0)
+	for _, u := range unique {
+		ri = append(ri, ReportImage{
+			Tag:        fmt.Sprintf("%s:%s", u.image, u.tag),
+			RepoDigest: u.digest,
+		})
+	}
+	return ri
 }
