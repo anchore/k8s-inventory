1df3e6aaf580ef9d2691ca532b35ff04d4f5a716
Bradley Jones <bradley.jones@anchore.com>
2024-05-02 13:56:13 +0100

    test: add missing test of valid anchore info
    Signed-off-by: Bradley Jones <bradley.jones@anchore.com>

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -4,9 +4,8 @@ import (
 	"flag"
 	"testing"
 
-	"github.com/spf13/viper"
-
 	"github.com/anchore/go-testutils"
+	"github.com/spf13/viper"
 )
 
 var update = flag.Bool("update", false, "update the *.golden files for config string output")
@@ -66,5 +65,57 @@ func TestSensitiveConfigString(t *testing.T) {
 	expected := string(testutils.GetGoldenFileContents(t))
 	if actual != expected {
 		t.Errorf("Config string does not match expected\nactual: %s\nexpected: %s", actual, expected)
+	}
+}
+
+func TestAnchoreInfo_IsValid(t *testing.T) {
+	type fields struct {
+		URL      string
+		User     string
+		Password string
+		Account  string
+		HTTP     HTTPConfig
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name: "valid",
+			fields: fields{
+				URL:      "http://anchore.example.com",
+				User:     "admin",
+				Password: "foobar",
+				Account:  "admin",
+				HTTP:     HTTPConfig{},
+			},
+			want: true,
+		},
+		{
+			name: "invalid",
+			fields: fields{
+				URL:      "http://anchore.example.com",
+				User:     "",
+				Password: "foobar",
+				Account:  "admin",
+				HTTP:     HTTPConfig{},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			anchore := &AnchoreInfo{
+				URL:      tt.fields.URL,
+				User:     tt.fields.User,
+				Password: tt.fields.Password,
+				Account:  tt.fields.Account,
+				HTTP:     tt.fields.HTTP,
+			}
+			if got := anchore.IsValid(); got != tt.want {
+				t.Errorf("AnchoreInfo.IsValid() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
